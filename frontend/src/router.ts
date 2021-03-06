216ff76a1fbd670bb6d821e35bdd2a8fa2fd7b7c
Matthew Gamble <git@matthewgamble.net>
2021-03-06 21:57:45 +1100

    Cleanup

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -15,14 +15,6 @@ const routes: Array<RouteRecordRaw> = [
     name: "corrections",
     component: Corrections,
   },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  // },
 ];
 
 const router = createRouter({
