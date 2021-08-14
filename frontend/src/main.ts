7850c4cc46848d3889d72328d6542a179d0d8ba6
Matthew Gamble <git@matthewgamble.net>
2021-08-15 00:05:53 +1000

    Finally remove vuex
    I never actually used it. Vue's new built-in 'reactive' hook provides a
    nice clean way to create lightweight stores that don't need much
    ceremony.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import router from "./router";
-import store from "./store";
 import { initCookie } from "./cookie";
 
 //import "@mdi/font/css/materialdesignicons.min.css";
@@ -10,8 +9,6 @@ import "./scss/app.scss";
 import AppRoot from "./AppRoot.vue";
 
 const app = createApp(AppRoot);
-// TODO: remove vuex
-app.use(store);
 app.use(router);
 
 initCookie(app);
